7705b350d7e7d3019dc27c403de9df235268cfe3
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-05-20 00:18:18 -0400

    made slight changes

--- ddpm.py
+++ ddpm.py
@@ -6,10 +6,8 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm
 from torch import optim
 from utils import *
-from modules import UNet
 import logging
 from torch.utils.tensorboard import SummaryWriter
-
 from modules import UNet
 
 logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=logging.INFO, datefmt='%I:%M:%S')
@@ -24,43 +22,43 @@ class Diffusion:
         self.device = device
 
         self.beta = self.prepare_noise_schedule().to(device)
-        self.alpha = 1 - self.beta
+        self.alpha = 1. - self.beta
         self.alpha_hat = torch.cumprod(self.alpha, dim=0)
 
     def prepare_noise_schedule(self):
         return torch.linspace(self.beta_start, self.beta_end, self.noise_steps)
 
     def noise_images(self, x, t):
-        sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:,None,None,None]
-        sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[:, None,None,None]
-        e = torch.rand_like(x)
-        return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * e, e
+        sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None, None]
+        sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[:, None, None, None]
+        Ɛ = torch.randn_like(x)
+        return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * Ɛ, Ɛ
 
     def sample_timesteps(self, n):
         return torch.randint(low=1, high=self.noise_steps, size=(n,))
 
     def sample(self, model, n):
-        logging.info(f"Sampling {n} new images...")
+        logging.info(f"Sampling {n} new images....")
         model.eval()
         with torch.no_grad():
             x = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device)
             for i in tqdm(reversed(range(1, self.noise_steps)), position=0):
                 t = (torch.ones(n) * i).long().to(self.device)
-                predicting_noise = model(x,t)
-                alpha = self.alpha[t][:,None,None,None]
-                alpha_hat = self.alpha_hat[t][:,None,None,None]
-                beta = self.beta[t][:,None,None,None]
+                predicted_noise = model(x, t)
+                alpha = self.alpha[t][:, None, None, None]
+                alpha_hat = self.alpha_hat[t][:, None, None, None]
+                beta = self.beta[t][:, None, None, None]
                 if i > 1:
                     noise = torch.randn_like(x)
                 else:
                     noise = torch.zeros_like(x)
-                # this algorithm is from the paper itself.
-                x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicting_noise) + torch.sqrt(beta) * noise
+                # this algorithm is from the paper
+                x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
         model.train()
-        # to get the values between 0 and 1
-        x = (x.clamp(-1,1) + 1) / 2
+        x = (x.clamp(-1, 1) + 1) / 2
         x = (x * 255).type(torch.uint8)
         return x
+
 
 def train(args):
     setup_logging(args.run_name)
@@ -103,7 +101,7 @@ def launch():
     args.epochs = 500
     args.batch_size = 12
     args.image_size = 64
-    args.dataset_path = r"datasets/Linnaeus 5 64X64/train/"
+    args.dataset_path = r"datasets/landscapes/"
     args.device = "cuda"
     args.lr = 3e-4
     train(args)
